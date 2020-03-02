a93fb6d170f54c1744db6640207e8a4d6c14c9c7
Tomas Tirlikas <tomastir@gmail.com>
2020-03-02 17:49:23 +0000

    added simple breadth first algorithm to visit as mary breweries within given traveling distance.

--- main.go
+++ main.go
@@ -14,39 +14,39 @@ import (
 )
 
 var (
-	dgraph = "127.0.0.1:9080"
-	lat    = flag.Float64("lat", 54.900518, "Latitude of initial location")
-	long   = flag.Float64("long", 23.893718, "Longitude of initial location")
+	dgraph          = "127.0.0.1:9080"
+	lat             = flag.Float64("lat", 54.900518, "Latitude of initial geoLocation")
+	long            = flag.Float64("long", 23.893718, "Longitude of initial geoLocation")
+	totalDistanceKm = distance(*flag.Float64("fuel", 2000,
+		"The distance that can be traveled before returning to the initial geoLocation"))
 )
 
 func main() {
 	startTimer := time.Now()
 	flag.Parse()
 
-	resp := queryBreweries(*lat, *long)
-	var beerTypes []string
-	var totalDistance float64
-	fmt.Printf("Found %d beer factories:\n", len(resp))
+	breweries := queryBreweries(*lat, *long, totalDistanceKm/2) // there must be enough fuel for a return trip.
+
+	err := breweries.findDistances()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Found %d beer factories:\n", len(breweries.orderedList))
 
 	fmt.Printf("\t -> HOME: %g %g distance 0km\n", *lat, *long)
-	for _, v := range resp {
-		beerTypes = append(beerTypes, v.Beers...)
 
-		// Calculate the distance
-		lat1 := v.Location.Coordinates[0]
-		long1 := v.Location.Coordinates[1]
-		distance := findDistance(*lat, *long, lat1, long1)
-		totalDistance += distance
-
-		fmt.Printf("\t -> [%d] %s: %g %g distance %.fkm\n", v.ID, v.Name, lat1,
-			long1, distance)
+	for _, v := range breweries.orderedList {
+		fmt.Printf("\t -> [%d] %s: %g %g distance %s\n", v.ID, v.Name, v.loc.lat, v.loc.long, v.distanceFromPrevious)
 	}
-	fmt.Printf("\t <- HOME: %g %g distance 0km\n", *lat, *long)
 
-	fmt.Printf("\nTotal distance travelled: %.fkm\n", totalDistance)
+	fmt.Printf("\t <- HOME: %g %g distance %s\n", *lat, *long, breweries.orderedList[len(breweries.orderedList)-1].distanceToHome)
 
-	fmt.Printf("\nCollected %d beer types:\n", len(beerTypes))
-	for _, v := range beerTypes {
+	fmt.Printf("\nTotal distance travelled: %s\n", breweries.totalDistance)
+
+	fmt.Printf("\nCollected %d beer types:\n", len(breweries.beerTypes))
+	for _, v := range breweries.beerTypes {
 		fmt.Printf("\t -> %s\n", v)
 	}
 
@@ -55,37 +55,80 @@ func main() {
 	fmt.Printf("\nProgram took: %s\n", timeLasted)
 }
 
-// findDistance returns distance in kilometers between two points on earth using Haversin formula.
-func findDistance(lat1, long1, lat2, long2 float64) float64 {
-	// convert to radians
-	var la1, lo1, la2, lo2, r float64
-	la1 = lat1 * math.Pi / 180
-	lo1 = long1 * math.Pi / 180
-	la2 = lat2 * math.Pi / 180
-	lo2 = long2 * math.Pi / 180
-	r = 6371009 // earth radius in meters
-
-	h := haversin(la2-la1) + math.Cos(la1)*math.Cos(la2)*haversin(lo2-lo1)
-
-	return (2 * r * math.Asin(math.Sqrt(h))) / 1000
-}
-
-// Haversin function.
-func haversin(theta float64) float64 {
-	return math.Pow(math.Sin(theta/2), 2)
+type breweries struct {
+	startingPoint geoLocation
+	radius        distance
+	list          map[int]*brewery
+	orderedList   []*brewery
+	totalDistance distance
+	beerTypes     []string
 }
 
 type brewery struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Location struct {
-		Coordinates []float64 `json:"coordinates"`
-	} `json:"location"`
-	Beers []string `json:"beers"`
+	ID    int
+	Name  string
+	loc   geoLocation
+	Beers []string
+
+	distanceToHome       distance
+	distanceFromPrevious distance
 }
 
-// Returns a list of breweries ir range of 1000km of given coordinates.
-func queryBreweries(lat float64, long float64) []brewery {
+func (b *breweries) findDistances() error {
+	if len(b.list) < 1 {
+		return fmt.Errorf("sorry, no breweries found")
+	}
+	var closest distance = 99999
+	b.orderedList = make([]*brewery, 1)
+	// Find which one  is closest to the starting point.
+	for _, v := range b.list {
+		d := v.loc.distanceTo(b.startingPoint)
+		//fmt.Println(d)
+		if d < closest {
+			closest = d
+			b.orderedList[0] = v
+			b.orderedList[0].distanceFromPrevious = d
+		}
+	}
+	b.totalDistance += closest
+	b.beerTypes = append(b.beerTypes, b.orderedList[0].Beers...)
+
+	delete(b.list, b.orderedList[0].ID)
+
+	for b.totalDistance+b.orderedList[len(b.orderedList)-1].distanceToHome < totalDistanceKm {
+
+		closest = 999999
+		br := brewery{}
+		// find the next closest factory
+		for _, v := range b.list {
+			d := v.loc.distanceTo(b.orderedList[len(b.orderedList)-1].loc)
+
+			if d < closest {
+				closest = d
+				br = *v
+				br.distanceFromPrevious = d
+				br.distanceToHome = v.loc.distanceTo(b.startingPoint)
+			}
+		}
+
+		// if total distance is more than total possible range, do not add to the list
+		if b.totalDistance+br.distanceToHome+closest > totalDistanceKm {
+			break
+		}
+		b.totalDistance += closest
+		b.orderedList = append(b.orderedList, &br)
+		b.beerTypes = append(b.beerTypes, br.Beers...)
+		delete(b.list, b.orderedList[len(b.orderedList)-1].ID)
+
+	}
+
+	b.totalDistance += b.orderedList[len(b.orderedList)-1].distanceToHome
+
+	return nil
+}
+
+// Returns a list of breweries in range of given distance (in kilometers) and starting coordinates.
+func queryBreweries(lat float64, long float64, radius distance) breweries {
 	conn, err := grpc.Dial(dgraph, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
@@ -96,29 +139,82 @@ func queryBreweries(lat float64, long float64) []brewery {
 
 	var q = fmt.Sprintf(`
 	{
-		breweries(func: near(location, [%v, %v], 1000000) ) {
-			id
-			name
-			location
-			beers
-		}
-	}
+  		breweries(func: near(location, [%v, %v], %d) )  {
+   			id
+    		name
+    		location
+    		beers
+  	}
+}
 	
-	`, lat, long)
+	`, lat, long, int(radius*1000)) // radius has to be converted to meters
 
 	resp, err := dg.NewTxn().Query(context.Background(), q)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var decode struct {
-		Breweries []brewery
+		Breweries []struct {
+			ID       int    `json:"id"`
+			Name     string `json:"name"`
+			Location struct {
+				Type        string    `json:"type"`
+				Coordinates []float64 `json:"coordinates"`
+			} `json:"location"`
+			Beers []string `json:"beers"`
+		} `json:"breweries"`
 	}
+	//fmt.Printf("%s", resp.Json)
 
 	if err := json.Unmarshal(resp.GetJson(), &decode); err != nil {
 		log.Fatal(err)
 	}
 
-	return decode.Breweries
+	//fmt.Println("returned breweries count: ", len(decode.Breweries))
+
+	brewMap := make(map[int]*brewery)
+	for _, v := range decode.Breweries {
+		brewMap[v.ID] = &brewery{
+			ID:   v.ID,
+			Name: v.Name,
+			loc: geoLocation{
+				lat:  v.Location.Coordinates[0],
+				long: v.Location.Coordinates[1],
+			},
+			Beers: v.Beers,
+		}
+	}
+
+	return breweries{startingPoint: geoLocation{lat, long}, radius: distance(radius), list: brewMap}
+}
+
+// distance holds distance in kilometers.
+type distance int
+
+func (d distance) String() string { return fmt.Sprintf("%dkm", d) }
+
+type geoLocation struct {
+	lat  float64
+	long float64
+}
+
+// distanceTo returns distance in kilometers between two points on earth using Haversin formula.
+func (g geoLocation) distanceTo(loc geoLocation) distance {
+	// convert to radians
+	var la1, lo1, la2, lo2, r float64
+	la1 = g.lat * math.Pi / 180
+	lo1 = g.long * math.Pi / 180
+	la2 = loc.lat * math.Pi / 180
+	lo2 = loc.long * math.Pi / 180
+	r = 6371009 // earth radius in meters
+
+	h := haversin(la2-la1) + math.Cos(la1)*math.Cos(la2)*haversin(lo2-lo1)
+
+	return distance((2 * r * math.Asin(math.Sqrt(h))) / 1000)
+}
+
+// Haversin function.
+func haversin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
 }
